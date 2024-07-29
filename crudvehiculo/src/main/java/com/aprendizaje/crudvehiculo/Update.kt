7d1d7247a8941795c70882e3c5fef917c006c6f2
JoseRodriguezCtec <i202115413@cibertec.edu.pe>
2024-07-29 10:32:10 -0500

    Add: Update && activity_update.xml

---
+++ crudvehiculo/src/main/java/com/aprendizaje/crudvehiculo/Update.kt
@@ -0,0 +1,14 @@
+package com.aprendizaje.crudvehiculo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Update : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update)
+    }
+}
