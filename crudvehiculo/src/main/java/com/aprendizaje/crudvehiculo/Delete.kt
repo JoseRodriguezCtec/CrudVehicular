e2ab158b1f0d23dc788f2e565d44060e4b69b008
JoseRodriguezCtec <i202115413@cibertec.edu.pe>
2024-08-02 15:36:47 -0500

    add: delete.xml

---
+++ crudvehiculo/src/main/java/com/aprendizaje/crudvehiculo/Delete.kt
@@ -0,0 +1,43 @@
+package com.aprendizaje.crudvehiculo
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.aprendizaje.crudvehiculo.databinding.ActivityDeleteBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class Delete : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDeleteBinding
+    private lateinit var databaseReference: DatabaseReference
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDeleteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.DeleteButton.setOnClickListener {
+            val VehiculoNumber = binding.VehicleDelete.text.toString()
+            if (VehiculoNumber.isEmpty()) {
+               deleteData(VehiculoNumber)
+            }else{
+                Toast.makeText(this, "Please Enter Vehicle Number", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun deleteData(VehiculoNumber: String) {
+        databaseReference = FirebaseDatabase.getInstance().getReference("VehiculoData")
+        databaseReference.child(VehiculoNumber).removeValue().addOnSuccessListener {
+            binding.VehicleDelete.text.clear()
+            Toast.makeText(this, "Data Deleted Successfully", Toast.LENGTH_SHORT).show()
+        }.addOnSuccessListener {
+            Toast.makeText(this, "Data Not Deleted", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
